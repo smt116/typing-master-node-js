594567763201bae29caabae925d33aeedaae79c2
Maciej Malecki <smt116@gmail.com>
2013-05-24 23:35:04 +0200

    Added less-middleware.

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    less = require('less-middleware'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  //app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(less({
+    src: __dirname + '/public',
+    compress: true
+  }));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
