9764dd497df2514a7a1c741322bfc53fc1c4b353
Maciej Malecki <smt116@gmail.com>
2013-05-26 21:43:49 +0200

    Text is getting from random record from mongo.

--- routes/play.js
+++ routes/play.js
@@ -1,3 +1,12 @@
-exports.index = function(req, res){
-  res.render('play');
+var model = require('../models/text');
+
+exports.index = function(req, res) {
+  var rand = Math.random();
+  var text = model.Text.findOne({
+    random: { $gte: rand }
+  }, function(err, object) {
+    res.render('play', {
+      text: object.text
+    });
+  });
 };
