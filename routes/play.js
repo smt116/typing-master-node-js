ca3c23028c79be0920b14d9e02136fe87066e877
Maciej Malecki <smt116@gmail.com>
2013-05-29 21:59:27 +0200

    Fixed draw from the database.
    Sometimes js pick random number that is bigger than all values in the 'random field' in collection so there wasn't any object and render tries call text on null object which throw an exception.

--- routes/play.js
+++ routes/play.js
@@ -1,10 +1,25 @@
 var model = require('../models/text');
 
-exports.index = function(req, res) {
+var getRandomItem = function(callback) {
   var rand = Math.random();
-  var text = model.Text.findOne({
+
+  model.Text.findOne({
     random: { $gte: rand }
   }, function(err, object) {
+    if(object === null) {
+      model.Text.findOne({
+        random: { $lte: rand }
+      }, function(err, object) {
+        callback(object);
+      });
+    } else {
+      callback(object);
+    }
+  });
+}
+
+exports.index = function(req, res) {
+  getRandomItem(function(object) {
     res.render('play', {
       text: object.text
     });
