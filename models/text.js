4d928cc7a9cd5ccc79e2b4f0a16164c76e9ca469
Maciej Malecki <smt116@gmail.com>
2013-05-29 23:40:45 +0200

    Moved fintRandomText function to model.

--- models/text.js
+++ models/text.js
@@ -1,4 +1,23 @@
 var mongoose = require("mongoose"),
     schema = require("../db/schema");
 
-exports.Text = mongoose.model("Text", schema.textSchema);
+mongoose.model('Text', schema.textSchema);
+exports.Text = mongoose.model("Text");
+
+exports.Text.findRandomText = function(callback) {
+  var rand = Math.random();
+
+  mongoose.model("Text").findOne({
+    random: { $gte: rand }
+  }, function(err, object) {
+    if(object === null) {
+      model.Text.findOne({
+        random: { $lte: rand }
+      }, function(err, object) {
+        callback(object);
+      });
+    } else {
+      callback(object);
+    }
+  });
+}
